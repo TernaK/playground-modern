f41b67b58b02439d5804fd2016c97fa96e5b54c2
Terna Kpamber <tkpamber@yahoo.com>
2017-02-21 22:40:10 -0800

    Use EBO, use local shaders, vec4 for fragment shader

--- playground-modern/fragment_shader.glsl
+++ playground-modern/fragment_shader.glsl
@@ -1,7 +1,7 @@
 #version 330 core
 
-out vec3 color;
+out vec4 color;
 
 void main() {
-  color = vec3(0.9, 0.1, 0.2);
+  color = vec4(0.9, 0.1, 0.2, 1.0);
 }
--- playground-modern/hellomodern.cpp
+++ playground-modern/hellomodern.cpp
@@ -9,7 +9,6 @@
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 #include <iostream>
-#include <fstream>
 #include <sstream>
 using namespace std;
 
@@ -19,54 +18,52 @@ void keyboardCallback(GLFWwindow* window, int key, int scancode, int action, int
   }
 }
 
-GLuint getShaderProgram(string vertexShaderPath, string fragmentShaderPath) {
-  
-  //vertex shader
-  ifstream ifs(vertexShaderPath);
-  stringstream ss;
-  ss << ifs.rdbuf();
-  ifs.close();
-  string vertexShaderSourceString = ss.str();
-  const char *vertexShaderSource = vertexShaderSourceString.c_str();
-  
-  GLuint vertexShader;
-  vertexShader = glCreateShader(GL_VERTEX_SHADER);
-  glShaderSource(vertexShader, 1, &vertexShaderSource, nullptr);
-  glCompileShader(vertexShader);
-  
+const char *vertexShaderString =
+"	#version 330 core\n"
+" layout (location = 0) in vec3 position;\n"
+" void main() {\n"
+"  gl_Position = vec4(position, 1.0);\n"
+" }" ;
+
+const char *fragmentShaderString =
+"	#version 330 core\n"
+" out vec3 color;\n"
+" void main() {\n"
+"  color = vec3(1.0, 0.5, 0.2);\n"
+" }" ;
+
+GLint getShaderProgram() {
   GLint success;
   GLchar infoLog[512];
+  
+  GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
+  glShaderSource(vertexShader, 1, &vertexShaderString, NULL);
+  glCompileShader(vertexShader);
   glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
   if(!success) {
     glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
-    cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << endl;
+    std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
   }
   
-  //fragment shader
-  ifs = ifstream(fragmentShaderPath);
-  ss.str("");
-  ss << ifs.rdbuf();
-  ifs.close();
-  string fragmentShaderSourceString = ss.str();
-  const char *fragmentShaderSource = fragmentShaderSourceString.c_str();
-  
-  GLuint fragmentShader;
-  fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
-  glShaderSource(fragmentShader, 1, &fragmentShaderSource, nullptr);
+  GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+  glShaderSource(fragmentShader, 1, &fragmentShaderString, NULL);
   glCompileShader(fragmentShader);
-  
   glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
   if(!success) {
     glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
-    cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << infoLog << endl;
+    std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << infoLog << std::endl;
   }
   
-  
-  GLuint shaderProgram;
-  shaderProgram = glCreateProgram();
+  GLint shaderProgram = glCreateProgram();
   glAttachShader(shaderProgram, vertexShader);
   glAttachShader(shaderProgram, fragmentShader);
   glLinkProgram(shaderProgram);
+  glGetProgramiv(shaderProgram, GL_COMPILE_STATUS, &success);
+  if(!success) {
+    glGetShaderInfoLog(shaderProgram, 512, NULL, infoLog);
+    std::cout << "ERROR::SHADER::PROGRAM::COMPILATION_FAILED\n" << infoLog << std::endl;
+  }
+  
   glDeleteShader(vertexShader);
   glDeleteShader(fragmentShader);
   
@@ -84,8 +81,8 @@ int main(int argc, char * argv[]) {
   glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
   glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
   glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-  glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
   glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
+  glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
   
   GLFWwindow *window = glfwCreateWindow(640, 480, "hello modern opengl", nullptr, nullptr);
   if(window == nullptr){
@@ -96,7 +93,7 @@ int main(int argc, char * argv[]) {
   glfwMakeContextCurrent(window);
   
   //glew
-  glewExperimental = true;
+  glewExperimental = GL_TRUE;
   if(glewInit() != GLEW_OK){
     puts("failed to create init glew");
     glfwTerminate();
@@ -109,40 +106,48 @@ int main(int argc, char * argv[]) {
   
   glfwSetKeyCallback(window, keyboardCallback);
   
-  
   //shader program
-  GLuint shaderProgram = getShaderProgram("/Users/Terna/Desktop/playground-modern/playground-modern/vertex_shader.glsl", "/Users/Terna/Desktop/playground-modern/playground-modern/fragment_shader.glsl");
+  GLuint shaderProgram = getShaderProgram();
   
   GLfloat vertices[] = {
     -0.5,-0.5,0,
     0.5,-0.5,0,
-    0,0.5,0
+    -0.5,0.5,0,
+    0.5,0.5,0
+  };
+  GLuint indices[] = {
+    0,1,2,
+    1,2,3
   };
   
-  GLuint VAO, VBO;
+  GLuint VAO, VBO, EBO;
   glGenVertexArrays(1, &VAO);
   glGenBuffers(1, &VBO);
+  glGenBuffers(1, &EBO);
   
   glBindVertexArray(VAO);
   {
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3*sizeof(GLfloat), (GLvoid*)0);
     glEnableVertexAttribArray(0);
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
   }
   glBindVertexArray(0);
   
   while(!glfwWindowShouldClose(window)){
     glfwPollEvents();
     
-    glClearColor(0.2, 0.3, 0.5, 1.0);
+    glClearColor(0.3, 0.4, 0.6, 1.0);
     glClear(GL_COLOR_BUFFER_BIT);
-    
     
     glUseProgram(shaderProgram);
     glBindVertexArray(VAO);
     {
-      glDrawArrays(GL_TRIANGLES, 0, 3);
+      glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+      glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
     }
     glBindVertexArray(0);
     
@@ -151,6 +156,7 @@ int main(int argc, char * argv[]) {
   
   glDeleteVertexArrays(1, &VAO);
   glDeleteBuffers(1, &VBO);
+  glDeleteBuffers(1, &EBO);
   glfwTerminate();
   return 0;
 }
