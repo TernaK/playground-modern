d5850c4ed1860de03908e0f922a97e6f1c29b55d
Terna <tkpamber@yahoo.com>
2017-03-07 11:08:10 -0800

    fix botched commit #2

--- playground-modern/ResourceManagerTest.cpp
+++ playground-modern/ResourceManagerTest.cpp
@@ -7,18 +7,53 @@
 //
 
 #include "ResourceManager.hpp"
-#include "Material.hpp"
+#include "Texture.hpp"
 #include "Shader.hpp"
 #include "AppTestUtility.hpp"
+using namespace std;
 
-int main(int argc, char * argv[])
+GLFWwindow *window;
+
+TEST(ShaderTest, TestInitialization)
 {
+  Shader s("resources/shaders/rm_vshader.glsl", "resources/shaders/rm_fshader.glsl");
+  ASSERT_GT(s.program, 0);
+}
+
+TEST(TextureTest, TestInitialization)
+{
+  Texture t("resources/textures/awesomeface.png");
+  ASSERT_GT(t.textureId, 0);
+}
+
+TEST(ResourceManagerTest, TestInitialization)
+{
+  Shader s("resources/shaders/rm_vshader.glsl", "resources/shaders/rm_fshader.glsl");
+  Texture t("resources/textures/awesomeface.png");
+  
+  ResourceManager rm;
+  
+  // catch non-existent shaders
+  rm.addShader(s, "shader");
+  rm.addTexture(t, "texture");
+  ASSERT_GT(rm.getShader("shader").program, 0);
+  ASSERT_EQ(rm.getShader("texture").program, 0);
+  ASSERT_GT(rm.getTexture("texture").textureId, 0);
+  ASSERT_EQ(rm.getTexture("shader").textureId, 0);
+  
+  // catch duplicate shaders
+  rm.addShader(s, "shader");
+  rm.addTexture(t, "texture");
+  ASSERT_EQ(rm.shaders.size(), 1);
+  ASSERT_EQ(rm.textures.size(), 1);
+}
+
+int main(int argc, char * argv[]) {
+  ::testing::InitGoogleTest(&argc, argv);
   
   window = glGetWindow();
   
-  int width, height;
-  glfwGetFramebufferSize(window, &width, &height);
-  glViewport(0, 0, width, height);
-  
   return RUN_ALL_TESTS();
 }
+
+
